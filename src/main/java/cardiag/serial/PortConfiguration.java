615892893205b4d03e8de677b3bda632ebeea549
David Matejcek <david.matejcek@i.cz>
2013-01-01 11:11:29 +0100

    Issue 3: Programmer can set his own serial port via maven properties
    - do it from command line via -D property or via profile in settings.xml
    - default is /dev/ttyUSB0

---
+++ src/main/java/cardiag/serial/PortConfiguration.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package cardiag.serial;
+
+
+/**
+ * @author David Matějček
+ *
+ */
+public class PortConfiguration {
+
+  private String portName;
+
+  public String getPortName() {
+    return portName;
+  }
+
+  public void setPortName(String portName) {
+    this.portName = portName;
+  }
+
+
+
+}
---
+++ src/test/java/cardiag/test/TestConfiguration.java
@@ -0,0 +1,49 @@
+/**
+ *
+ */
+package cardiag.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+
+import cardiag.serial.PortConfiguration;
+
+
+/**
+ * @author David Matějček
+ */
+public class TestConfiguration {
+
+  private Properties properties;
+
+
+  public TestConfiguration() {
+    final InputStream stream = TestConfiguration.class.getClassLoader().getResourceAsStream("test.properties");
+    try {
+      Properties properties = new Properties();
+      properties.load(stream);
+      this.properties = properties;
+    } catch (IOException e) {
+      Assert.fail("Cannot load test.properties file");
+    } finally {
+      IOUtils.closeQuietly(stream);
+    }
+  }
+
+
+  public String getPortName() {
+    return properties.getProperty("serialPort");
+  }
+
+
+  public PortConfiguration getPortConfiguration() {
+    final PortConfiguration cfg = new PortConfiguration();
+    cfg.setPortName(getPortName());
+    return cfg;
+  }
+
+}
