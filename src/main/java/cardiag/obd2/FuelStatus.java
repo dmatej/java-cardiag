0c522400d4fd54d260ebfaedd3e5e21227f1b4d0
David Matejcek <david.matejcek@i.cz>
2014-08-02 17:17:10 +0200

    Fixed Fuel Status decoding

--- src/main/java/cardiag/obd2/FuelStatus.java
+++ src/main/java/cardiag/obd2/FuelStatus.java
@@ -8,12 +8,13 @@ package cardiag.obd2;
  */
 public enum FuelStatus {
 
-  OPEN_LOW_TEMP(0x00, "Open loop due to insufficient engine temperature"), CLOSED_USING_OXYGEN(0x01,
-      "Closed loop, using oxygen sensor feedback to determine fuel mix"), OPEN_LOAD(0x02,
-      "Open loop due to engine load OR fuel cut due to deceleration"), OPEN_FAILURE(0x03,
-      "Open loop due to system failure"), CLOSED_FAILURE_FEEDBACK(0x04,
-      "Closed loop, using at least one oxygen sensor but there is a fault in the feedback system"), ZERO5(0x05,
-      "Always zero"), ZERO6(0x06, "Always zero"), ZERO7(0x07, "Always zero"), ;
+  OPEN_LOW_TEMP(1, "Open loop due to insufficient engine temperature"), //
+  CLOSED_USING_OXYGEN(2, "Closed loop, using oxygen sensor feedback to determine fuel mix"), //
+  OPEN_LOAD(4, "Open loop due to engine load OR fuel cut due to deceleration"), //
+  OPEN_FAILURE(8, "Open loop due to system failure"), //
+  CLOSED_FAILURE_FEEDBACK(16,
+      "Closed loop, using at least one oxygen sensor but there is a fault in the feedback system"), //
+  ZERO5(32, "Always zero"), ZERO6(0x06, "Always zero"), ZERO7(0x07, "Always zero"), ;
 
   final int code;
   final String description;
